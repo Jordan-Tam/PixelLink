216b212ef67d95f654b895ebb840ed1c0bbf3a59
Sofia Martinez <sofiacaro1284@gmail.com>
2025-04-14 23:27:37 -0400

    set up index.js

---
+++ data/index.js
@@ -0,0 +1,7 @@
+import userDataFunctions from './users.js';
+import gameDataFunctions from './games.js';
+import commentDataFunctions from './comments.js';
+
+export const userData = userDataFunctions;
+export const gameData = gameDataFunctions;
+export const commentData = commentDataFunctions;
