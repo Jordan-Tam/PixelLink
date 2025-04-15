937d73b9331aa67d120c4e11bc203c5cb5cdf5e4
Jordan <58477084+Jordan-Tam@users.noreply.github.com.>
2025-04-15 16:38:27 -0400

    Login page

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,12 @@ const constructorMethod = (app) => {
             stylesheet: "/public/css/landing-page.css"
         });
     });
+    app.get("/login", (req, res) => {
+        res.render('login', {
+            title: "Login",
+            stylesheet: "/public/css/login.css"
+        });
+    });
     app.get("/error", (req, res) => {
         res.sendFile(path.resolve("static/error.html"));
     });
