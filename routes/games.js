4c5de1ad485517244a15a46b59d05a9d636be060
Sofia Martinez <sofiacaro1284@gmail.com>
2025-04-27 20:11:32 -0400

    wrote games get routes

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import {checkString} from '../helpers.js';
-
+import games from '../data/games.js';
 
 const router = Router();
 
@@ -9,15 +9,42 @@ router.route('/')
     .post()
 
 router.route('/list')
-    .get()
+    .get(async (req, res) => {
+        try {
+            const gamesList = await games.getAllGames();
+            return res.render('game-list', {games: gamesList});
+        } catch (error) {
+            return res.send(error);
+        }
+    })
     .post()
 
 router.route('/:id')
-    .get()
+    .get(async (req, res) => {
+        try{
+            if(!req.params.id){
+                return res.status(400).json({error: "Invalid game id"});
+            }
+            const game = await games.getGameById(req.params.id);
+            return res.render('game-page', {game: game});
+        } catch (error) {
+            return res.send(error);
+        }
+    })
     .post()
 
 router.route('/:id/form')
-    .get()
+    .get(async (req, res) => {
+        try{
+            if(!req.params.id){
+                return res.status(400).json({error: "Invalid game id"});
+            }
+            const game = await games.getGameById(req.params.id);
+            return res.render('game-form', {game: game});
+        } catch (error) {
+            return res.send(error);
+        }
+    })
     .post()
 
 export default router;
