97446b4f99d7bd6a66ed6d2fe8d902dd9f456264
Sofia Martinez <sofiacaro1284@gmail.com>
2025-04-29 14:47:21 -0400

    wrote user/:id/friends post route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import {checkString} from '../helpers.js';
+import {checkString, checkId} from '../helpers.js';
 import users from '../data/users.js';
 
 
@@ -37,7 +37,7 @@ router.route('/:id')
     .post(async (req, res) => {
         try {
             const id = checkString(req.params.id, 'User id','POST user/:id');
-            const {username, email, password} = req.body;
+            let {username, email, password} = req.body;
             const nUsername = checkString(username, 'Username', 'POST user/:id');
             const nEmail = checkString(email, 'Email', 'POST user/:id');
             const nPassword = checkString(password, 'Password', 'POST user/:id');
@@ -68,6 +68,32 @@ router.route('/:id/friends')
             return res.status(500).json({error});
         }
     })
-    .post();
+    .post(async (req, res) => {
+        try {
+            const id = checkId(req.params.id, "POST user/:id/friends", "User");
+            let {friendId} = req.body;
+            friendId = checkId(friendId, "POST user/:id/friends", "User");
+            if(!req.session.user || req.session.user._id != id){ //checks if user is visiting their own profile page
+                //render the 403 error
+                return res.status(403).json({error: "Permission Denied"});
+            }
+            await users.addFriend(id, friendId); //add friend
+            const user = await users.getUserById(id); //get user
+            
+            let friends = user.friends; //list of user's friends' ids
+            let friendsArr = [];
+            for(let id of friends){
+                const friend = await users.getUserById(id);
+                friendsArr.push(friend); //array of users(friends)
+            }
+            return res.render('friends-list', {
+                friends: friendsArr,
+                username: user.username,
+                title: `${user.username}'s Friends List`
+            });
+        } catch (error) {
+            return res.status(500).json({error});
+        }
+    });
 
 export default router;
