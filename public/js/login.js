dc87c5aca7a5bba48ee732706ad5ebdaa1e3dda3
Jimmy Colgan <jcolgan1@stevens.edu>
2025-04-29 18:24:46 -0400

    client js clean up

--- public/js/login.js
+++ public/js/login.js
@@ -10,90 +10,27 @@ if (loginForm) {
         // Rehide the error paragraph.
         error.hidden = true;
 
-        // Prevent the default behavior of the form.
-        event.preventDefault();
-
         let username_input = username.value.trim();
         let password_input = password.value;
 
-        // Email validation.
-        if (!email_input || email_input.length === 0) {
-            error.innerHTML = "Please enter a valid email.";
-            error.hidden = false;
-            return;
-        }
-
-        if(email_input.includes('..')){
-            error.innerHTML = "Invalid email address. (two periods in a row)"
-            error.hidden = false;
-            return;
-        }
-
-        let atSymbol = false;
-        let period = false;
-        let after_period_count = 0;
-        for (let char of  email_input){
-            if (!atSymbol){
-                if (char === '@'){ 
-                    atSymbol = true;
-                    break;
-                }
-                else if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.-".indexOf(char) < 0) {
-                        error.innerHTML = "Invalid email address."
-                        error.hidden = false;
-                        return;
-                }
-            } else {
-
-                if (!period){
-
-                    if (char === "."){
-                        period = true;
-                    }
-                    else if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-".indexOf(char) < 0){
-                            error.innerHTML = "Invalid email domain."
-                            error.hidden = false;
-                            return;
-                    } 
-                    
-                } else {
-
-                    after_period_count++;
-                    if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".indexOf(char) < 0){
-                        error.innerHTML = "Invalid email domain."
-                        error.hidden = false;
-                        return;
-                    } 
-                
-
-                }
-            
-            }
-        }
-
-        if (after_period_count < 2 || !period ){
-            error.innerHTML = "Invalid email domain."
-            error.hidden = false;
-            return;
-        }
-
-
         // Username validation.
-        if (!username_input || username_input.trim().length === 0) {
+        if (!username_input || username_input.length === 0) {
+            event.preventDefault();
             error.innerHTML = "Username must be between 3-15 characters long and only include letters, numbers, periods, underscores, and minus signs.";
             error.hidden = false;
             return;
         }
 
-        if (username.charAt(0) === '.' || username.charAt(username.length - 1) === '.'){
+        if (username_input.charAt(0) === '.' || username_input.charAt(username_input.length - 1) === '.'){
+            event.preventDefault();
             error.innerHTML = "Username cannot start or end with a period.";
             error.hidden = false;
             return;
         }
 
-        username_input = username_input.trim();
         for (let char of username_input) {
             if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.-".indexOf(char) < 0) {
+                event.preventDefault();
                 error.innerHTML = "Username must be between 3-15 characters long and only include letters, numbers, periods, underscores, and minus signs.";
                 error.hidden = false;
                 return;
@@ -107,11 +44,12 @@ if (loginForm) {
         }
 
         const characters = {lower: 0, upper: 0, special: 0, number: 0};    
-        for (let i = 0; i < password.length; i++){
+        for (let i = 0; i < password_input.length; i++){
             if (password_input.charCodeAt(i) >= 48 && password_input.charCodeAt(i) <= 57) characters['number']++;
             else if (password_input.charCodeAt(i) >= 65 && password_input.charCodeAt(i) <= 90) characters['upper']++;
             else if (password_input.charCodeAt(i) >= 97 && password_input.charCodeAt(i) <= 122) characters['lower']++;
             else if (password_input.charCodeAt(i) === 32){
+                event.preventDefault();
                 error.innerHTML = "Spaces not allowed in password";
                 error.hidden = false;
                 return;
@@ -119,7 +57,8 @@ if (loginForm) {
             else characters['special']++;                  
         }                       
         if (characters['lower'] === 0 || characters['upper'] === 0 || characters['special'] === 0 || characters['number'] === 0){
-            error.innerHTML = "Password must contain 1+ uppercase, 1+ lowercase, 1+ number, and 1+ special character ";
+            event.preventDefault();
+            error.innerHTML = "Password must contain 1+ uppercase, 1+ lowercase, 1+ number, and 1+ special character";
             error.hidden = false;
             return;
         }
--- public/js/registration.js
+++ public/js/registration.js
@@ -8,136 +8,148 @@ let confirmPassword = document.getElementById("confirmPassword");
 
 if (registrationForm) {
     registrationForm.addEventListener('submit', (event) => {
-
         // Rehide the error paragraph.
-        error.hidden = true;
+    error.hidden = true;
 
         // Prevent the default behavior of the form.
-        event.preventDefault();
 
-        let email_input = email.value.trim();
-        let username_input = username.value.trim();
-        let password_input = password.value;
-        let confirmPassword_input = confirmPassword.value;
+    let email_input = email.value.trim();
+    let username_input = username.value.trim();
+    let password_input = password.value;
+    let confirmPassword_input = confirmPassword.value;
+
 
         // Email validation.
-        if (!email_input || email_input.length === 0) {
-            error.innerHTML = "Please enter a valid email.";
-            error.hidden = false;
-            return;
-        }
+    if (!email_input || email_input.length === 0) {
+        event.preventDefault();
+        error.innerHTML = "Please enter a valid email.";
+        error.hidden = false;
+        return;
+    }
 
-        if(email_input.includes('..')){
-            error.innerHTML = "Invalid email address. (two periods in a row)"
-            error.hidden = false;
-            return;
-        }
+    if(email_input.includes('..')){
+        event.preventDefault();
+        error.innerHTML = "Invalid email address. (two periods in a row)"
+        error.hidden = false;
+        return;
+    }
 
-        let atSymbol = false;
-        let period = false;
-        let after_period_count = 0;
-        for (let char of  email_input){
-            if (!atSymbol){
-                if (char === '@'){ 
-                    atSymbol = true;
-                    break;
+    let atSymbol = false;
+    let period = false;
+    let after_period_count = 0;
+    for (let char of  email_input){
+        if (!atSymbol){
+            if (char === '@'){ 
+                atSymbol = true;
+            }
+            else if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.-".indexOf(char) < 0) {
+                    event.preventDefault();
+                    error.innerHTML = "Invalid email address."
+                    error.hidden = false;
+                    return;
+            }
+        } else {
+
+            if (!period){
+
+                if (char === "."){
+                    period = true;
                 }
-                else if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.-".indexOf(char) < 0) {
-                        error.innerHTML = "Invalid email address."
-                        error.hidden = false;
-                        return;
-                }
-            } else {
-
-                if (!period){
-
-                    if (char === "."){
-                        period = true;
-                    }
-                    else if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-".indexOf(char) < 0){
-                            error.innerHTML = "Invalid email domain."
-                            error.hidden = false;
-                            return;
-                    } 
-                    
-                } else {
-
-                    after_period_count++;
-                    if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".indexOf(char) < 0){
+                else if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-".indexOf(char) < 0){
+                        event.preventDefault();
                         error.innerHTML = "Invalid email domain."
                         error.hidden = false;
                         return;
-                    } 
+                } 
                 
+            } else {
 
-                }
+                after_period_count++;
+                if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".indexOf(char) < 0){
+                    event.preventDefault();
+                    error.innerHTML = 'Invalid email domain.'
+                    error.hidden = false;
+                    return;
+                } 
             
+
             }
+        
         }
+    }
 
-        if (after_period_count < 2 || !period ){
-            error.innerHTML = "Invalid email domain."
-            error.hidden = false;
-            return;
-        }
+    if (after_period_count < 2 || !period ){
+        event.preventDefault();
+        error.innerHTML = "Invalid email domain."
+        error.hidden = false;
+        return;
+    }
+        
+        //username validation 
+        
+    if (!username_input || username_input.length === 0) {
+        event.preventDefault();
+        error.innerHTML = "Username must be between 3-15 characters long and only include letters, numbers, periods, underscores, and minus signs.";
+        error.hidden = false;
+        return;
+    }
 
+    if (username_input.charAt(0) === '.' || username_input.charAt(username_input.length - 1) === '.'){
+        event.preventDefault();
+        error.innerHTML = "Username cannot start or end with a period.";
+        error.hidden = false;
+        return;
+    }
 
-        if (!username_input || username_input.trim().length === 0) {
+    for (let char of username_input) {
+        if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.-".indexOf(char) < 0) {
+            event.preventDefault();
             error.innerHTML = "Username must be between 3-15 characters long and only include letters, numbers, periods, underscores, and minus signs.";
             error.hidden = false;
             return;
         }
+    }
 
-        if (username.charAt(0) === '.' || username.charAt(username.length - 1) === '.'){
-            error.innerHTML = "Username cannot start or end with a period.";
+    // Confirm password validation.
+    if (password_input !== confirmPassword_input) {
+        event.preventDefault();
+        error.innerHTML = "Password and Confirm Password fields do not match."
+        error.hidden = false;
+        return;
+    }
+
+    //Password validation
+    if (password_input.length < 8){
+        event.preventDefault();
+        error.innerHTML = "Password must be 8+ characters."
+        error.hidden = false;
+        return;
+    }
+
+    const characters = {lower: 0, upper: 0, special: 0, number: 0};    
+    for (let i = 0; i < password_input.length; i++){
+        if (password_input.charCodeAt(i) >= 48 && password_input.charCodeAt(i) <= 57) characters['number']++;
+        else if (password_input.charCodeAt(i) >= 65 && password_input.charCodeAt(i) <= 90) characters['upper']++;
+        else if (password_input.charCodeAt(i) >= 97 && password_input.charCodeAt(i) <= 122) characters['lower']++;
+        else if (password_input.charCodeAt(i) === 32){
+            event.preventDefault();
+            error.innerHTML = "Spaces not allowed in password";
             error.hidden = false;
             return;
         }
+        else characters['special']++;                  
+    }     
+    
 
-        username_input = username_input.trim();
-        for (let char of username_input) {
-            if ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.-".indexOf(char) < 0) {
-                error.innerHTML = "Username must be between 3-15 characters long and only include letters, numbers, periods, underscores, and minus signs.";
-                error.hidden = false;
-                return;
-            }
-        }
 
-        // Confirm password validation.
-        if (password_input !== confirmPassword_input) {
-            error.innerHTML = "Password and Confirm Password fields do not match."
-            error.hidden = false;
-            return;
-        }
-
-        //Password validation
-        if (password_input.length < 8){
-            error.innerHTML = "Password must be 8+ characters."
-            error.hidden = false;
-            return;
-        }
-
-        const characters = {lower: 0, upper: 0, special: 0, number: 0};    
-        for (let i = 0; i < password.length; i++){
-            if (password_input.charCodeAt(i) >= 48 && password_input.charCodeAt(i) <= 57) characters['number']++;
-            else if (password_input.charCodeAt(i) >= 65 && password_input.charCodeAt(i) <= 90) characters['upper']++;
-            else if (password_input.charCodeAt(i) >= 97 && password_input.charCodeAt(i) <= 122) characters['lower']++;
-            else if (password_input.charCodeAt(i) === 32){
-                error.innerHTML = "Spaces not allowed in password";
-                error.hidden = false;
-                return;
-            }
-            else characters['special']++;                  
-        }                       
-        if (characters['lower'] === 0 || characters['upper'] === 0 || characters['special'] === 0 || characters['number'] === 0){
-            error.innerHTML = "Password must contain 1+ uppercase, 1+ lowercase, 1+ number, and 1+ special character ";
-            error.hidden = false;
-            return;
-        }
-            
-
-        registrationForm.reset();
-        error.innerHTML = "";
-
+    if (characters['lower'] === 0 || characters['upper'] === 0 || characters['special'] === 0 || characters['number'] === 0){
+        event.preventDefault();
+        error.innerHTML = "Password must contain 1+ uppercase, 1+ lowercase, 1+ number, and 1+ special character";
+        error.hidden = false;
+        return;
+    }
+        
+    registrationForm.reset();
+    error.innerHTML = "";
     })
 }
